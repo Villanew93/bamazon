12c7f4853f52092e33886631843c12e16c52b552
isai villanueva~git configssh-keygen -t rsa -b 4096 -C isai.villanu.7@gmail.comgit config --global user.name isai <isai.villanu.7@gmail.com>
2019-09-17 14:59:49 -0700

    finished tables

--- bamazon.sql
+++ bamazon.sql
@@ -7,6 +7,7 @@ USE bamazon;
 CREATE TABLE products (
 item_id INTEGER NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(10) NOT NULL,
+department_name VARCHAR(40) NOT NULL,
 price DECIMAL(10,2) NOT NULL,
 stock_quantity INTEGER(10) NOT NULL,
 PRIMARY KEY (item_id)
